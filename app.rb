2278ec2f5641ec204a0d657df5da2684ca6c9619
Pierre Baillet <pierre@baillet.name>
2012-07-01 23:54:26 +0200

    Mostly works. HAML is not very nice though...

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 
 require 'sinatra'
 require 'haml'
+require 'linguist'
 
+module Linguist
+  # Language bayesian classifier.
+  class Classifier
+    attr_reader :languages
+  end
+end
+  
 
 get '/' do
-	haml :index
+  @code = ''
+  haml :index
+end
+
+post '/paste' do
+  @code = params[:code]
+  classifier = Linguist::Classifier.instance
+  @scores = classifier.classify(@code)
+
+  haml :index
 end
 
